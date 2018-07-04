bed16e9aa539fff71df288bfb4855208491534ec
Ben <allobon@gmail.com>
2018-07-04 22:31:06 +0100

    messging around with servers

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 const Mailgun = require("mailgun-js");
 const twilio = require("twilio")("ACd74eaa07213fb7ebcfb142b597bccd2f", "8f593b438c35c95ec7068e3143bbbd73");
 const mailgun = new Mailgun({
@@ -12,18 +12,26 @@ const port = process.env.PORT || 1337;
 // set up app to understand json in post requests & route index page to something
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "vohzd.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get("/",(req, res) => {
   res.send("peanut butter jelly time");
 });
 
+app.post("/test", (req, res) => {
 
+  let mgData = {
+    from: req.body.from,
+    to: req.body.to,
+    subject: req.body.subject,
+    html: req.body.html
+  };
+  mailgun.messages().send(mgData, (err, body) => {
+    console.log(body);
+    res.send("OK");
+  });
+
+})
 
 app.post("/submit", (req, res, next) => {
 
@@ -35,21 +43,26 @@ app.post("/submit", (req, res, next) => {
   };
 
   mailgun.messages().send(mgData, (err, body) => {
+    console.log(body);
     if (err){
       res.render("error", { error: err });
     }
     else {
-      twilio.messages.create({
-        from: "+442380000598",
-        to: "+447805031421",
-        body: req.body.html
-      }).then((message) => {
-        res.send("success");
-      }).catch((err) => {
-        console.log(err);
-      })
+      res.send("OK");
     }
   });
+
+  /*
+    twilio.messages.create({
+      from: "+442380000598",
+      to: "+447805031421",
+      body: req.body.html
+    }).then((message) => {
+      res.send("success");
+    }).catch((err) => {
+      console.log(err);
+    })*/
+
 });
 
 app.listen(port, () => {
